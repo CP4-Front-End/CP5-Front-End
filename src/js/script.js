50f0e05ae093faad96206cd37b57ea188f730720
Joao Gonzales <gonzales.jpgc@gmail.com>
2026-03-31 21:01:33 -0300

    feat: Adicionando funcionalidades no player

--- src/js/script.js
+++ src/js/script.js
@@ -19,14 +19,13 @@ const likeIcon = document.getElementById("likeIcon");
 let isPlaying = false;
 let isLiked = false;
 
-// PLAY / PAUSE
 playBtn.addEventListener("click", () => {
-  isPlaying = !isPlaying;
-
-  if (isPlaying) {
+  if (audio.paused) {
+    audio.play();
     playIcon.classList.remove("fa-play");
     playIcon.classList.add("fa-pause");
   } else {
+    audio.pause();
     playIcon.classList.remove("fa-pause");
     playIcon.classList.add("fa-play");
   }
@@ -53,4 +52,65 @@ buttons.forEach(btn => {
     playIcon.classList.remove("fa-play");
     playIcon.classList.add("fa-pause");
   });
+});
+
+const audio = document.getElementById("audioPlayer");
+
+// Banco de dados das músicas
+const musics = [
+  "/src/audio/Amiri.mp3",
+  "/src/audio/TODASASLUZES.mp3",
+  "/src/audio/ESQV.mp3",
+  "/src/audio/DBN.mp3"
+];
+
+let currentIndex = 0;
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+
+    currentIndex = index;
+
+    const music = btn.getAttribute("data-src");
+
+    audio.src = music;
+    audio.play();
+
+    player.classList.remove("translate-y-full");
+
+    playIcon.classList.remove("fa-play");
+    playIcon.classList.add("fa-pause");
+  });
+});
+
+
+
+const nextBtn = document.getElementById("next");
+
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+
+  if (currentIndex >= musics.length) {
+    currentIndex = 0; // volta pro início
+  }
+
+  audio.src = musics[currentIndex];
+  audio.play();
+
+  playIcon.classList.replace("fa-play", "fa-pause");
+});
+
+const prevBtn = document.getElementById("prev");
+
+prevBtn.addEventListener("click", () => {
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = musics.length - 1; // vai pro final
+  }
+
+  audio.src = musics[currentIndex];
+  audio.play();
+
+  playIcon.classList.replace("fa-play", "fa-pause");
 });
